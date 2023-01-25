098e34296776f283dfe573931e657c6d8b222ab3
Sajan <sajan123poudel4@gmail.com>
2023-01-25 01:01:50 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Log.v("hello","aa ")
+            Toast.makeText(this,"Hello! Thank You for pressing me.",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
