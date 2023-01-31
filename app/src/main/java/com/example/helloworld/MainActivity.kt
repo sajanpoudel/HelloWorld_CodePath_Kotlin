afce180ba2d8b64c3af3919188de8c5fc8564798
sajanpoudel <sajan123poudel4@gmail.com>
2023-01-30 23:45:30 -0500

    Two buttons added

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -9,13 +9,18 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
+        val button1 = findViewById<Button>(R.id.button)
+        button1.setOnClickListener{
             Log.v("hello","aa ")
             Toast.makeText(this,"Hello! Thank You for pressing me.",Toast.LENGTH_SHORT).show()
+        }
+        val button2 = findViewById<Button>(R.id.buttongreet)
+        button2.setOnClickListener{
+            Toast.makeText(this,"Hi! Good Morning.", Toast.LENGTH_SHORT).show()
         }
     }
 }
